f2f01a6b4b6107d675b0ac4cbf4a4437c4ee6969
DavydovDenys <denvanrain@gmail.com>
2019-12-16 23:24:12 +0200

    add navigation titles

--- app.rb
+++ app.rb
@@ -6,3 +6,11 @@ require 'sinatra/reloader'
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
+get '/about' do
+  erb "<h1>Мы лучшие!</h1>"
+end
+
+get '/signin' do
+  "Hello World"
+end
